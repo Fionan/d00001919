50470f7c8510284956596c65d2e7a5dd365d0f34
fionan <Fionan@users.noreply.github.com>
2023-05-30 20:03:36 +0100

    Add files via upload
    Adding the GPO scripts

---
+++ powershell_cmds/Staff_logon_GPO.ps1
@@ -0,0 +1,28 @@
+﻿# Creating a Logon restriction policy
+
+New-GPO -Name "Staff_Login_policy" -comment "For setting logon hours of members of Staff"
+
+#set GPO as variable 
+
+$GPO = Get-GPO -Name "Staff_Login_Policy"
+
+#Set login hours to  allow  based on this table but in Dec
+# Sunday: 00000000, 00000000,  00000000,
+# Monday: 00000000, 01111111,  10000000,
+# Tuesday: 00000000, 01111111,  10000000,
+# Wednesday: 00000000, 01111111,  10000000,
+# Thursday: 00000000, 01111111,  10000000,
+# Friday: 00000000, 01111111,  10000000,
+# Saturday: 00000000, 00000000,  00000000,
+
+# 0,0,0 for weekends & 0,127,128 ofr says of the week
+
+
+$logonhours=@{"Logonhours"= [byte[]]$hours=@(0, 0, 0, 0, 127, 128, 0, 127, 128, 0, 127, 128, 0, 127, 128, 0, 127, 128, 0, 0, 0)}
+
+Set-GPRegistryValue -Guid $GPO.Id -Key "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -ValueName "LogonHours" -Type Binary -Value $LogonHours.LogonHours
+
+
+# Apply to other departments
+Set-GPPermissions -Guid $GPO.Id -TargetName insurancedepartment -TargetType Group -PermissionLevel GpoApply
+Set-GPPermissions -Guid $GPO.Id -TargetName ticketingdepartment -TargetType Group -PermissionLevel GpoApply
---
+++ powershell_cmds/Travel_Logon_GPO.ps1
@@ -0,0 +1,42 @@
+﻿
+
+#Insure Group policy is actually installed
+Install-WindowsFeature GPMC
+
+
+# Creating a Logon restriction policy
+
+New-GPO -Name "Travel_Login_policy" -comment "For setting logon hours of members of travel group"
+
+#set GPO as variable 
+
+$GPO = Get-GPO -Name "Travel_Login_Policy"
+
+#Set login hours to  allow  based on this table but in Dec
+# Sunday: 00000000, 11111111,  11110000,
+# Monday: 00000000, 11111111,  11110000,
+# Tuesday: 00000000, 11111111,  11110000,
+# Wednesday: 00000000, 11111111,  11110000,
+# Thursday: 00000000, 11111111,  11110000,
+# Friday: 00000000, 11111111,  11110000,
+# Saturday: 00000000, 11111111,  11110000,
+
+
+# So each day is 0,255,240
+
+$logonhours=@{"Logonhours"= [byte[]]$hours=@(0, 255, 240, 0, 255, 240, 0, 255, 240, 0, 255, 240, 0, 255, 240, 0, 255, 240, 0, 255, 240, 0, 255, 240)}
+
+
+# Hope this works!!
+Set-GPRegistryValue -Guid $GPO.Id -Key "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -ValueName "LogonHours" -Type Binary -Value $LogonHours.LogonHours
+
+
+# Lets create a report   
+Get-GPOReport $GPO.id -ReportType XML -Path "C:\Users\Administrator\scripts\Test_report.xml"
+
+# We can view it here:
+notepad.exe "C:\Users\Administrator\scripts\Test_report.xml"
+
+
+# Edit as needed - applypermssion
+Set-GPPermissions -Guid $GPO.Id -TargetName traveldepartment -TargetType Group -PermissionLevel GpoApply
