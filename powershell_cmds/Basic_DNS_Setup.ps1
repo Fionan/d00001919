8070b1b9a3c8cbbb68539de735228f2fc8974f82
fionan <Fionan@users.noreply.github.com>
2023-05-30 16:00:58 +0100

    Add files via upload
    Adding DNS script

---
+++ powershell_cmds/Basic_DNS_Setup.ps1
@@ -0,0 +1,11 @@
+﻿###DNS###
+
+#not needed on AD/DNS one pc only
+#Get-WindowsFeature -Name DNS
+#Install-WindowsFeature -Name DNS -IncludeManagementTools
+#Add-DnsServerPrimaryZone -Name "Mediholidays.local" -ReplicationScope "Forest"
+
+Add-DnsServerResourceRecordA -Name "dhcp" -ZoneName "Mediholidays.local" -IPv4Address "192.168.1.2"
+
+
+###End of DNS### 
