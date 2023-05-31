036979f2df68f817ee7db33b4fff4ef703e0ea59
fionan <Fionan@users.noreply.github.com>
2023-05-31 14:21:05 +0100

    Update Basic_DNS_Setup.ps1
    DNS file updated

--- powershell_cmds/Basic_DNS_Setup.ps1
+++ powershell_cmds/Basic_DNS_Setup.ps1
@@ -7,5 +7,8 @@
 
 Add-DnsServerResourceRecordA -Name "dhcp" -ZoneName "Mediholidays.local" -IPv4Address "192.168.1.2"
 
+Add-DnsServerResourceRecordA -Name "web" -ZoneName "Mediholidays.local" -IPv4Address "192.168.1.12"
+
+Add-DnsServerResourceRecordA -Name "files" -ZoneName "Mediholidays.local" -IPv4Address "192.168.1.15"
 
 ###End of DNS### 
